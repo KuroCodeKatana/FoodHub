20ef48d7b24ffceebf30bbd7b4883c72edfcf5fe
Sukanya Pal <sukanya96pal@gmail.com>
2020-09-19 21:20:33 +0530

    Admin_Item compelete

--- FoodHub/Controllers/Admin_ItemController.cs
+++ FoodHub/Controllers/Admin_ItemController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FoodHub.Models;
+using System.Security.Cryptography;
 
 namespace FoodHub.Controllers
 {
@@ -16,34 +17,39 @@ namespace FoodHub.Controllers
         {
             ItemEntry VE = new ItemEntry();
             VE.ITEMLIST = (from a in DB.ITEM
-                               select new ITEMLIST()
-                               {
-                                   ITEM_CD = a.ITEM_CD,
-                                   ITEM_NM = a.ITEM_NM,
-                                   ITEM_DESC = a.ITEM_DESC,
-                                   IMG=a.IMG,
-                                   PRICE=a.PRICE,
-                                   STATUS=a.STATUS,
-                                   CATE_CD=a.CATE_CD,
-                                   ITEM_TYPE= a.ITEM_TYPE
-                               }).ToList();
+                           select new ITEMLIST()
+                           {
+                               ITEM_CD = a.ITEM_CD,
+                               ITEM_NM = a.ITEM_NM,
+                               ITEM_DESC = a.ITEM_DESC,
+                               IMG = a.IMG,
+                               PRICE = a.PRICE,
+                               STATUS = a.STATUS,
+                               CATE_CD = a.CATE_CD,
+                               ITEM_TYPE = a.ITEM_TYPE
+                           }).ToList();
             for (int i = 0; i <= VE.ITEMLIST.Count - 1; i++)
             {
                 VE.ITEMLIST[i].SLNO = Convert.ToInt32(i + 1);
             }
             return View(VE);
         }
-        public JsonResult Save(CategoryEntry VE)
+        public JsonResult Save(ItemEntry VE)
         {
             try
             {
-                CATEGORY CAT = new CATEGORY();
+                ITEM CAT = new ITEM();
                 Random rnd = new Random();
                 int id = rnd.Next(0, 9999);
-                CAT.CATE_CD = id;
-                CAT.CATE_NM = VE.CATEGORY.CATE_NM;
-                CAT.CATE_DESC = VE.CATEGORY.CATE_DESC;
-                DB.CATEGORY.Add(CAT);
+                CAT.ITEM_CD = id;
+                CAT.ITEM_NM = VE.ITEM.ITEM_NM;
+                CAT.ITEM_DESC = VE.ITEM.ITEM_DESC;
+                CAT.IMG = VE.ITEM.IMG;
+                CAT.PRICE = VE.ITEM.PRICE;
+                CAT.STATUS = VE.ITEM.STATUS;
+                CAT.CATE_CD = VE.ITEM.CATE_CD;
+                CAT.ITEM_TYPE = VE.ITEM.ITEM_TYPE;
+                DB.ITEM.Add(CAT);
                 DB.SaveChanges();
                 return Json("Success", JsonRequestBehavior.AllowGet);
             }
@@ -53,24 +59,49 @@ namespace FoodHub.Controllers
             }
 
         }
-        public JsonResult GetCategory(int CateCode)
+        public JsonResult GetItem(int ItemCode)
         {
-            var Q = DB.CATEGORY.Where(M => M.CATE_CD == CateCode).FirstOrDefault();
-            return Json(Q, JsonRequestBehavior.AllowGet);
+            try
+            {
+                var Q = (from a in DB.ITEM
+                         select new
+                         {
+                             ITEM_CD = a.ITEM_CD,
+                             ITEM_NM = a.ITEM_NM,
+                             ITEM_DESC = a.ITEM_DESC,
+                             IMG = a.IMG,
+                             PRICE = a.PRICE,
+                             STATUS = a.STATUS,
+                             CATE_CD = a.CATE_CD,
+                             ITEM_TYPE = a.ITEM_TYPE
+
+                         }).FirstOrDefault();
+                
+                return Json(Q, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message + ex.InnerException, JsonRequestBehavior.AllowGet);
+            }
         }
-        public JsonResult Update(CategoryEntry VE)
+        public JsonResult Update(ItemEntry VE)
         {
-            int code = VE.CATEGORY.CATE_CD;
-            var Q = DB.CATEGORY.Where(M => M.CATE_CD == code).FirstOrDefault();
-            Q.CATE_NM = VE.CATEGORY.CATE_NM;
-            Q.CATE_DESC = VE.CATEGORY.CATE_DESC;
+            int code = VE.ITEM.ITEM_CD;
+            var Q = DB.ITEM.Where(M => M.ITEM_CD == code).FirstOrDefault();
+            Q.ITEM_NM = VE.ITEM.ITEM_NM;
+            Q.ITEM_DESC = VE.ITEM.ITEM_DESC;
+            Q.IMG = VE.ITEM.IMG;
+            Q.PRICE = VE.ITEM.PRICE;
+            Q.STATUS = VE.ITEM.STATUS;
+            Q.CATE_CD = VE.ITEM.CATE_CD;
+            Q.ITEM_TYPE = VE.ITEM.ITEM_TYPE;
             DB.SaveChanges();
             return Json("Success", JsonRequestBehavior.AllowGet);
         }
-        public JsonResult DeleteUser(int CateCode)
+        public JsonResult DeleteUser(int ItemCode)
         {
-            var Q = DB.CATEGORY.Where(M => M.CATE_CD == CateCode).FirstOrDefault();
-            DB.CATEGORY.Remove(Q);
+            var Q = DB.ITEM.Where(M => M.ITEM_CD == ItemCode).FirstOrDefault();
+            DB.ITEM.Remove(Q);
             DB.SaveChanges();
             return Json("Success", JsonRequestBehavior.AllowGet);
         }
