e3f3721dabae70e54d7ee840de271a9770e00d42
Sukanya Pal <sukanya96pal@gmail.com>
2020-09-25 18:25:27 +0530

    Menu view controlleer

--- FoodHub/Controllers/HomeController.cs
+++ FoodHub/Controllers/HomeController.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FoodHub.Models;
+using FoodHub.ViewModels;
 
 namespace FoodHub.Controllers
 {
     public class HomeController : Controller
     {
         //Default Home Page
+        DbModel DB = new DbModel();
+
         public ActionResult Index()
         {
+
             return View();
         }
         public ActionResult Menu()
         {
-            return View();
+            ItemEntry VE = new ItemEntry();
+            VE.ITEMLIST = (from test in DB.ITEM
+                     select new ITEMLIST
+                     {
+                         ITEM_CD = test.ITEM_CD,
+                         ITEM_NM=test.ITEM_NM,
+                         ITEM_DESC=test.ITEM_DESC,
+                         IMG=test.IMG,
+                         PRICE=test.PRICE,
+                         STATUS=test.STATUS,
+                         CATE_CD=test.CATE_CD,
+                         ITEM_TYPE=test.ITEM_TYPE
+                     }).ToList();
+            
+            return View(VE);
         }
         public ActionResult Contact()
         {
