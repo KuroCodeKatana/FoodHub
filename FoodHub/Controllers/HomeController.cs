bc2fee98adbba5549cebf385c008314d3df69d41
prabir1.paul.2019@gmail.com <prabir1.paul.2019@gmail.com>
2020-11-02 22:14:42 +0530

    T

--- FoodHub/Controllers/HomeController.cs
+++ FoodHub/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.EnterpriseServices;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -32,26 +33,23 @@ namespace FoodHub.Controllers
                                     CATE_CD = test.CATE_CD,
                                     ITEM_TYPE = test.ITEM_TYPE
                                 }).ToList();
-
+            for (int i = 0; i <= VE.BREAKFASTLIST.Count - 1; i++)
+            {
+                if (VE.BREAKFASTLIST[i].IMG != null)
+                {
+                    string filenm = VE.BREAKFASTLIST[i].IMG;
+                    string filepath = Server.MapPath("~/Content/ItemImage/" + filenm.ToString());
+                    VE.BREAKFASTLIST[i].IMG_FILE = DocpathToBase64(filepath);
+                    var mimetype = MimeMapping.GetMimeMapping(filenm);
+                    var mimeurl = "data:" + mimetype + ";base64,";
+                    VE.BREAKFASTLIST[i].IMG_FILE = (mimeurl + VE.BREAKFASTLIST[i].IMG_FILE);
+                    //VE.ITEMLIST[i].IMG_FILE = "~/Content/ItemImage/" + VE.ITEMLIST[i].IMG;
+                }
+            }
             VE.LUNCHLIST = (from test in DB.ITEM
-                                join b in DB.CATEGORY on test.CATE_CD equals b.CATE_CD
-                                where b.CATE_NM.ToUpper() == "LUNCH" && test.STATUS == "Available"
-                            select new ITEMLIST
-                                {
-                                    ITEM_CD = test.ITEM_CD,
-                                    ITEM_NM = test.ITEM_NM,
-                                    ITEM_DESC = test.ITEM_DESC,
-                                    IMG = test.IMG,
-                                    PRICE = test.PRICE,
-                                    STATUS = test.STATUS,
-                                    CATE_CD = test.CATE_CD,
-                                    ITEM_TYPE = test.ITEM_TYPE
-                                }).ToList();
-
-            VE.DINNERLIST = (from test in DB.ITEM
                             join b in DB.CATEGORY on test.CATE_CD equals b.CATE_CD
-                            where b.CATE_NM.ToUpper() == "DINNER" && test.STATUS == "Available"
-                             select new ITEMLIST
+                            where b.CATE_NM.ToUpper() == "LUNCH" && test.STATUS == "Available"
+                            select new ITEMLIST
                             {
                                 ITEM_CD = test.ITEM_CD,
                                 ITEM_NM = test.ITEM_NM,
@@ -62,6 +60,46 @@ namespace FoodHub.Controllers
                                 CATE_CD = test.CATE_CD,
                                 ITEM_TYPE = test.ITEM_TYPE
                             }).ToList();
+            for (int i = 0; i <= VE.LUNCHLIST.Count - 1; i++)
+            {
+                if (VE.LUNCHLIST[i].IMG != null)
+                {
+                    string filenm = VE.LUNCHLIST[i].IMG;
+                    string filepath = Server.MapPath("~/Content/ItemImage/" + filenm.ToString());
+                    VE.LUNCHLIST[i].IMG_FILE = DocpathToBase64(filepath);
+                    var mimetype = MimeMapping.GetMimeMapping(filenm);
+                    var mimeurl = "data:" + mimetype + ";base64,";
+                    VE.LUNCHLIST[i].IMG_FILE = (mimeurl + VE.LUNCHLIST[i].IMG_FILE);
+                    //VE.ITEMLIST[i].IMG_FILE = "~/Content/ItemImage/" + VE.ITEMLIST[i].IMG;
+                }
+            }
+            VE.DINNERLIST = (from test in DB.ITEM
+                             join b in DB.CATEGORY on test.CATE_CD equals b.CATE_CD
+                             where b.CATE_NM.ToUpper() == "DINNER" && test.STATUS == "Available"
+                             select new ITEMLIST
+                             {
+                                 ITEM_CD = test.ITEM_CD,
+                                 ITEM_NM = test.ITEM_NM,
+                                 ITEM_DESC = test.ITEM_DESC,
+                                 IMG = test.IMG,
+                                 PRICE = test.PRICE,
+                                 STATUS = test.STATUS,
+                                 CATE_CD = test.CATE_CD,
+                                 ITEM_TYPE = test.ITEM_TYPE
+                             }).ToList();
+            for (int i = 0; i <= VE.DINNERLIST.Count - 1; i++)
+            {
+                if (VE.DINNERLIST[i].IMG != null)
+                {
+                    string filenm = VE.DINNERLIST[i].IMG;
+                    string filepath = Server.MapPath("~/Content/ItemImage/" + filenm.ToString());
+                    VE.DINNERLIST[i].IMG_FILE = DocpathToBase64(filepath);
+                    var mimetype = MimeMapping.GetMimeMapping(filenm);
+                    var mimeurl = "data:" + mimetype + ";base64,";
+                    VE.DINNERLIST[i].IMG_FILE = (mimeurl + VE.DINNERLIST[i].IMG_FILE);
+                    //VE.ITEMLIST[i].IMG_FILE = "~/Content/ItemImage/" + VE.ITEMLIST[i].IMG;
+                }
+            }
 
             return View(VE);
         }
@@ -88,9 +126,36 @@ namespace FoodHub.Controllers
         {
             return View();
         }
-        public ActionResult Cart()
+        public JsonResult Cart(ItemEntry VE,int itemcd)
         {
-            return View();
+            try
+            {
+
+                Random rnd = new Random();
+                int id = rnd.Next(0, 9999);
+
+                CART CART = new CART();
+                CART.CART_ID = id;
+                CART.USER_ID = Convert.ToInt32(Session["USER_ID"].ToString());
+                CART.STATUS = "";
+                CART.CART_TIME = System.DateTime.Now;
+                DB.CART.Add(CART);
+                DB.SaveChanges();
+
+                CART_ITEM CARTITEM = new CART_ITEM();
+                CARTITEM.CI_ID = id;
+                CARTITEM.CART_ID = CART.CART_ID;
+                CARTITEM.ITEM_CD = itemcd;
+                CARTITEM.QNTY = 1;
+                DB.CART_ITEM.Add(CARTITEM);
+                DB.SaveChanges();
+
+                return Json("Success", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message + ex.InnerException, JsonRequestBehavior.AllowGet);
+            }
         }
         public ActionResult Single_Item()
         {
@@ -101,6 +166,20 @@ namespace FoodHub.Controllers
             VE.ITEM = item;
             return View(VE);
 
+        }
+        public string DocpathToBase64(string path)
+        {
+            try
+            {
+                var Docpath = Path.Combine(path);
+                byte[] imageBytes = System.IO.File.ReadAllBytes(Docpath);
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
         }
 
     }
